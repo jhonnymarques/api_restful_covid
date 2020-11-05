fb010875261dba5202719973e511b780b0c5f5fa
jhonnymarques <jhonnymarques.r@gmail.com>
2020-11-04 22:21:31 -0300

    criado controller cidade

---
+++ app/Http/Controllers/API/CitieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CitieController extends Controller
+{
+    public function index()
+    {
+        return "Cidades";
+    }
+}
