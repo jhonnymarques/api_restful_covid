bd73f7a6904903084742607d58561a9e5b8b8e87
jhonnymarques <jhonnymarques.r@gmail.com>
2020-11-04 22:21:51 -0300

    criada a rota cidades

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CitieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/ok', function () {
-    return ['status' => true];
-});
+Route::get('/cities', [CitieController::class, 'index']);
