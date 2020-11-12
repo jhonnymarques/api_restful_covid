b1eb0f7465006d1b5373d59f06fd63fe81e03579
jhonnymarques <jhonnymarques.r@gmail.com>
2020-11-11 21:52:32 -0300

    Criada a rota de listaem de bairros

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CitieController;
+use App\Http\Controllers\API\NeighborhoodController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/cities', [CitieController::class, 'index']);
+Route::get('/bairros', [NeighborhoodController::class, 'index']);
